9613e7f234373841103be2b88489c68131b04395
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-03-15 18:06:28 +0600

    Set up project.

---
+++ AlbumsLayout/Sources/App/Controllers/AlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumsViewController.swift
+//  AlbumsLayout
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 15.03.2023.
+//
+
+import UIKit
+
+final class AlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Albums"
+    }
+}
